0100d871cf26a7872ebbc89da043a17f6efe9ac4
Eric Turner <eric.turner@ditto.live>
2023-12-13 12:04:14 -0800

    Migrate to DQL v1 on SDK 4.5.0
    - update subscription, observer, and all queries to new DQL
    - updates to UI/UX
    - update README

---
+++ Tasks/Env.swift
@@ -0,0 +1,12 @@
+                                        
+import Foundation
+
+// This file is auto generated. Do not edit this, and edit .env instead.
+
+struct Env {
+    static let DITTO_APP_ID = "10bbfa5e-80f2-4bdc-9673-ba3ea6cb6396"
+    static let DITTO_PLAYGROUND_TOKEN = "49cad3ad-26ae-4be6-89d7-16fdeea74f5f"
+    static let DITTO_AUTH_PASSWORD = ""
+    static let DITTO_AUTH_PROVIDER = ""
+    static let DITTO_OFFLINE_TOKEN = ""
+}
