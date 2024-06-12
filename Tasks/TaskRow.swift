c98f9620862db76a3573a3dc58a91e4dcfafd136
Eric Turner <eric@magicwavesoftware.com>
2024-06-12 09:44:59 -0700

    Migrate to DQL v1 on SDK 4.7.1 (#5)
    * Migrate to DQL v1
    
    - update subscription, observer, and all queries to new DQL
    - updates to UI/UX
    - update README
    - update Env build phase params. Update SDK to 4.7.1

--- Tasks/TaskRow.swift
+++ Tasks/TaskRow.swift
@@ -8,43 +8,41 @@
 import SwiftUI
 
 struct TaskRow: View {
+    let task: TaskModel
 
-    let task: Task
-
-    var onToggle: ((_ task: Task) -> Void)?
-    var onClickBody: ((_ task: Task) -> Void)?
-    var onClickInvite: ((_ task: Task) -> Void)?
-
+    var onToggle: ((_ task: TaskModel) -> Void)?
+    var onClickBody: ((_ task: TaskModel) -> Void)?
+    var onClickInvite: ((_ task: TaskModel) -> Void)?
 
     var body: some View {
         HStack {
             Image(systemName: task.isCompleted ? "circle.fill": "circle")
                 .renderingMode(.template)
                 .foregroundColor(.accentColor)
+                .frame(minWidth: 32)
                 .onTapGesture {
                     onToggle?(task)
                 }
             if task.isCompleted {
                 Text(task.body)
                     .strikethrough()
-                    .onTapGesture {
-                        onClickBody?(task)
-                    }
-
             } else {
                 Text(task.body)
-                    .onTapGesture {
-                        onClickBody?(task)
-                    }
             }
+            
             Spacer()
+            
             Text(task.invitationIds.keys.reduce("", { x, y in
-                x +  (y ?? "") + ", "
+                x + y + ", "
             })).foregroundColor(Color.gray)
             
             Text("+").onTapGesture {
                 onClickInvite?(task)
             }
+        }
+        .contentShape(Rectangle())
+        .onTapGesture {
+            onClickBody?(task)
         }
     }
 }
@@ -52,9 +50,11 @@ struct TaskRow: View {
 struct TaskRow_Previews: PreviewProvider {
     static var previews: some View {
         List {
-            TaskRow(task: Task(body: "Get Milk", isCompleted: true, invitationIds: ["Susan": true, "John": true]))
-            TaskRow(task: Task(body: "Do Homework", isCompleted: false, invitationIds: [:]))
-            TaskRow(task: Task(body: "Take out trash", isCompleted: true, invitationIds: [:]))
+            TaskRow(task: TaskModel(
+                body: "Get Milk", isCompleted: true, invitationIds: ["Susan": true, "John": true])
+            )
+            TaskRow(task: TaskModel(body: "Do Homework"))
+            TaskRow(task: TaskModel(body: "Take out trash", isCompleted: true))
         }
     }
 }
